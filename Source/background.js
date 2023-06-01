12452579ea590c31dc7c9650036b53f6b6c1e026
Eric Portis <e@ericportis.com>
2023-05-31 20:55:24 -0700

    First failed try

--- Source/background.js
+++ Source/background.js
@@ -1,30 +1,31 @@
-let activeTabs = new Set();
-chrome.action.onClicked.addListener((tab) => {
-  if (activeTabs.has(tab.id)) {
-    activeTabs.delete(tab.id);
-    chrome.action.setIcon({
-      tabId: tab.id,
-      path: {
-        "16": "icon16.png",
-        "32": "icon32.png",
-        "48": "icon48.png",
-        "128": "icon128.png"
-      }
-    });
-  } else {
-    activeTabs.add(tab.id);
-    chrome.action.setIcon({
-      tabId: tab.id,
-      path: {
-        "16": "icon16-active.png",
-        "32": "icon32-active.png",
-        "48": "icon48-active.png",
-        "128": "icon128-active.png"
-      }
-    });
-  }
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['content_script.js']
-  });
-});
+// let activeTabs = new Set();
+// chrome.action.onClicked.addListener((tab) => {
+//   if (activeTabs.has(tab.id)) {
+//     activeTabs.delete(tab.id);
+//     chrome.action.setIcon({
+//       tabId: tab.id,
+//       path: {
+//         "16": "icon16.png",
+//         "32": "icon32.png",
+//         "48": "icon48.png",
+//         "128": "icon128.png"
+//       }
+//     });
+//   } else {
+//     activeTabs.add(tab.id);
+//     chrome.action.setIcon({
+//       tabId: tab.id,
+//       path: {
+//         "16": "icon16-active.png",
+//         "32": "icon32-active.png",
+//         "48": "icon48-active.png",
+//         "128": "icon128-active.png"
+//       }
+//     });
+//   }
+//   chrome.scripting.executeScript({
+//     target: {tabId: tab.id},
+//     files: ['content_script.js']
+//   });
+// });
+chrome.action.onClicked.addListener(function(tab) { (function(){!function(){var e=document.createElement("script");e.id="respimagelint-script",e.type="text/javascript",e.src="https://ausi.github.io/respimagelint/collector.js?"+Date.now(),document.body.appendChild(e)}();})() });
